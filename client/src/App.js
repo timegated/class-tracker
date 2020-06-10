6caebfdea8e99ec9d7e852f7a2e105bc2f7565b2
dbeccaria826 <beccdaniel@gmail.com>
2020-06-10 10:16:55 -0400

    setting up login and sign up UI, linking it to server side

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,30 @@ import Navbar from "./components/Layout/Navbar";
 import CurrentItems from "./components/Items/CurrentItems/CurrentItems";
 import ItemPriority from "./components/Items/ItemPriority/ItemPriority";
 import PlayerState from "./context/player/PlayerState";
-// import Login from "./components/Auth/Login";
-// import SignUp from "./components/Auth/SignUp";
+import AuthState from "./context/auth/AuthState";
+import Login from "./components/Auth/Login";
+import SignUp from "./components/Auth/SignUp";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
     return (
-        <PlayerState>
-            <Router>
-                <div className="main-content">
-                    <Navbar />
-                    <Switch>
-                        <Route path="/" exact component={Home} />
-                        <Route path="/about" exact component={About} />
-                        <Route path="/currentitems" exact component={CurrentItems} />
-                        <Route path="/priority" exact component={ItemPriority} />
-                        {/* <Route path="/login" exact component={Login} /> */}
-                        {/* <Route path="/signup" exact component={SignUp} /> */}
-                    </Switch>
-                </div>
-            </Router>
-        </PlayerState>
-        
+        <AuthState>
+            <PlayerState>
+                <Router>
+                    <div className="main-content">
+                        <Navbar />
+                        <Switch>
+                            <Route path="/" exact component={Home} />
+                            <Route path="/about" exact component={About} />
+                            <Route path="/currentitems" exact component={CurrentItems} />
+                            <Route path="/priority" exact component={ItemPriority} />
+                            <Route path="/login" exact component={Login} />
+                            <Route path="/signup" exact component={SignUp} />
+                        </Switch>
+                    </div>
+                </Router>
+            </PlayerState>
+        </AuthState>
     );
 };
 
--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -6,13 +6,12 @@ const Navbar = () => {
     return (
         <nav className="main-menu">
            <ul className="main-menu__items">
-               <li className="main-menu__item"><Link to="/" >Home</Link></li>
+                <li className="main-menu__item"><Link to="/" >Home</Link></li>
                 <li className="main-menu__item"><Link to="/about">About</Link></li>
                 <li className="main-menu__item"><Link to="/currentitems">Current Items</Link></li>
                 <li className="main-menu__item"><Link to="/priority">Priority</Link></li>
-
-               {/* <li className="main-menu__item"><Link to="/login">Login</Link></li> */}
-               {/* <li className="main-menu__item"><Link to="/signup">Sign Up</Link></li> */}
+                <li className="main-menu__item"><Link to="/login">Login</Link></li>
+                <li className="main-menu__item"><Link to="/signup">Sign Up</Link></li>
            </ul> 
         </nav>
     );

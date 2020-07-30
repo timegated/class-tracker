aaf94d83339107bad24898c15659f0bbb0f3a606
dbeccaria826 <beccdaniel@gmail.com>
2020-07-30 17:56:20 -0400

    api/itemdb: creating working routes for finding items with wow-classic-item api

--- server/dist/resources/itemDB/itemDB.router.js
+++ server/dist/resources/itemDB/itemDB.router.js
@@ -13,10 +13,24 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 const router = (0, _express.Router)();
 router.get('/', (req, res) => {
-  const raidItems = _itemDB.default.filter(i => i.itemLevel >= 75 && i.requiredLevel === 60 && i.quality === 'Epic');
+  const raidItems = _itemDB.default.filter(i => i.itemLevel >= 66 && i.itemLevel <= 80 && i.contentPhase <= 5 && i.requiredLevel === 60 && i.quality === 'Epic').sort((a, b) => {
+    return a.itemLevel - b.itemLevel;
+  });
 
   try {
-    res.status(200).json(raidItems);
+    res.status(200).json(raidItems.map(raidItem => {
+      return {
+        name: raidItem.name,
+        icon: raidItem.icon,
+        quality: raidItem.quality,
+        class: raidItem.class,
+        subclass: raidItem.subclass,
+        slot: raidItem.slot,
+        itemLevel: raidItem.itemLevel,
+        uniqueName: raidItem.uniqueName,
+        contentPhase: raidItem.contentPhase
+      };
+    }));
   } catch (error) {
     console.error(error);
     res.status(400).json({
@@ -26,18 +40,21 @@ router.get('/', (req, res) => {
 });
 router.get('/:name', (req, res) => {
   try {
-    const item = _itemDB.default.filter(i => {
-      if (i.uniqueName === req.params.name) {
-        return {
-          name: i.name,
-          icon: i.icon
-        };
-      }
-    });
+    const item = _itemDB.default.filter(i => i.uniqueName === req.params.name);
 
-    res.status(200).json({
-      item
-    });
+    res.status(200).json(item.map(i => {
+      return {
+        name: i.name,
+        icon: i.icon,
+        quality: i.quality,
+        class: i.class,
+        subclass: i.subclass,
+        slot: i.slot,
+        itemLevel: i.itemLevel,
+        uniqueName: i.uniqueName,
+        contentPhase: i.contentPhase
+      };
+    }));
   } catch (error) {
     console.error(error);
     res.status(400).json({
@@ -45,5 +62,24 @@ router.get('/:name', (req, res) => {
     });
   }
 });
+router.get('/:subclass'), (req, res) => {
+  try {
+    const subClass = _itemDB.default.filter(i => i.subclass === req.params.subclass);
+
+    res.status(200).json(subClass.map(i => {
+      return {
+        name: i.name,
+        icon: i.icon,
+        quality: i.quality,
+        class: i.class,
+        subclass: i.subclass,
+        slot: i.slot,
+        itemLevel: i.itemLevel,
+        uniqueName: i.uniqueName,
+        contentPhase: i.contentPhase
+      };
+    }));
+  } catch (error) {}
+};
 var _default = router;
 exports.default = _default;
--- server/src/resources/itemDB/itemDB.router.js
+++ server/src/resources/itemDB/itemDB.router.js
@@ -4,11 +4,23 @@ import items from "./itemDB";
 const router = Router();
 
 router.get('/', (req, res) => {
-  const raidItems = items.filter(i => i.itemLevel >= 75 && i.requiredLevel === 60 && i.quality === 'Epic').sort((a, b) => {
+  const raidItems = items.filter(i => i.itemLevel >= 66 && i.itemLevel <= 80 && i.contentPhase <= 5 && i.requiredLevel === 60 && i.quality === 'Epic').sort((a, b) => {
     return a.itemLevel - b.itemLevel;
   });
   try {
-    res.status(200).json(raidItems);
+    res.status(200).json(raidItems.map(raidItem => {
+      return {
+        name: raidItem.name,
+        icon: raidItem.icon,
+        quality: raidItem.quality,
+        class: raidItem.class,
+        subclass: raidItem.subclass,
+        slot: raidItem.slot,
+        itemLevel: raidItem.itemLevel,
+        uniqueName: raidItem.uniqueName,
+        contentPhase: raidItem.contentPhase
+      }
+    }));
   } catch (error) {
     console.error(error);
     res.status(400).json({ msg: "Something\'s amiss with / route" });
@@ -17,18 +29,45 @@ router.get('/', (req, res) => {
 
 router.get('/:name', (req, res) => {
   try {
-    const item = items.filter(i => {
-      if (i.uniqueName === req.params.name) {
-        return {
-          name: i.name,
-          icon: i.icon
-        }
+    const item = items.filter(i => i.uniqueName === req.params.name);
+    res.status(200).json(item.map(i => {
+      return {
+        name: i.name,
+        icon: i.icon,
+        quality: i.quality,
+        class: i.class,
+        subclass: i.subclass,
+        slot: i.slot,
+        itemLevel: i.itemLevel,
+        uniqueName: i.uniqueName,
+        contentPhase: i.contentPhase   
       }
-    });
-    res.status(200).json({ item });
+    }));
   } catch (error) {
     console.error(error);
     res.status(400).json({ msg: "Something bad happened with name route" });
   }
 });
+
+router.get('/:subclass'), (req, res) => {
+  try {
+    const subClass = items.filter(i => i.subclass === req.params.subclass.toLowerCase());
+    res.status(200).json(subClass.map(i => {
+      return {
+        name: i.name,
+        icon: i.icon,
+        quality: i.quality,
+        class: i.class,
+        subclass: i.subclass,
+        slot: i.slot,
+        itemLevel: i.itemLevel,
+        uniqueName: i.uniqueName,
+        contentPhase: i.contentPhase   
+      }
+    }))
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ msg: "Something bad happened with name route" });
+  }
+}
 export default router;

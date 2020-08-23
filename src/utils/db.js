e76cc34d488842cfe9d15832fe615787764dd6ba
dbeccaria826 <beccdaniel@gmail.com>
2020-08-23 16:21:51 -0400

    removed console logs from db.js in utils

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 import options from "../config";
 
 export const connect = (url = options.dbUrl, opts = {}) => {
-  console.log(options)
-  console.log(typeof url, url);
   console.log({ connectDB: "MongoDb connected" });
   return mongoose.connect(url, {
     ...opts,
